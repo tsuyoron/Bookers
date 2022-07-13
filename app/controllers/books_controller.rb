ab060b0cc12f3c0ef69feaa6eee4ee9d6bac3d6f
tsuyoron <108647290+tsuyoron@users.noreply.github.com>
2022-07-13 08:27:28 +0000

    [Add] routesの追記

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,13 @@
 class BooksController < ApplicationController
+  def index_new
+    
+  end
+  
+  def show
+    
+  end
+  
+  def edit
+    
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  get 'homes/top'
+  get '/' => 'homes#top'
+  get 'books' => 'books#index_new'
+  get 'books/:id' => 'books#show'
+  get 'books/:id/edit' => 'books#edit'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
