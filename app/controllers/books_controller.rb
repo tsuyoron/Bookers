e0a44e4519331e4636891a0f63199d6713d687d8
tsuyoron <108647290+tsuyoron@users.noreply.github.com>
2022-07-14 23:56:03 +0000

    [Add] 登録一覧表示機能の追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,21 +1,34 @@
 class BooksController < ApplicationController
   def index_new
-    
+    @book = Book.new
+    @books = Book.all
   end
-  
+
   def create
-    
+    #データを受け取り新規登録するためのインスタンス作成
+    book = Book.new(book_params)
+    #データをデータベースに保存するためのsaveメソッド実行
+    book.save
+    #詳細画面へリダイレクト
+    redirect_to "/books/#{book.id}"
   end
-  
+
   def show
-    
+
   end
-  
+
   def edit
-    
+
   end
-  
+
   def destroy
-    
+
   end
+
+  private
+  #ストロングパラメータ
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
 end
